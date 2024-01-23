c04680b1011ed68e4f6742b2d4c8bd8794bc138a
Andrey Ageew <faceofdeviler@gmail.com>
2024-01-23 13:07:32 +0300

    Added timezone.utc to avoid time conflict and made nicer output

--- RateFetcher/views.py
+++ RateFetcher/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from .models import CurrencyRequest
 from .services import get_currency_data
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 
 def get_current_usd(request):
@@ -12,7 +12,7 @@ def get_current_usd(request):
     # Проверяем, когда был сделан последний запрос
     last_request_time = CurrencyRequest.objects.last().time if CurrencyRequest.objects.exists() else None
 
-    if last_request_time and datetime.now() - last_request_time < timedelta(seconds=10):
+    if last_request_time and datetime.now(timezone.utc) - last_request_time < timedelta(seconds=10):
         # Если с последнего запроса прошло менее 10 секунд
         rate = CurrencyRequest.objects.last().rate
     else:
@@ -25,7 +25,18 @@ def get_current_usd(request):
     # Получаем 10 последних запросов
     last_requests = CurrencyRequest.objects.order_by('-time')[:10].values('time', 'rate')
 
-    return JsonResponse({
+    # Форматирование данных для ответа
+    last_requests_formatted = [
+        {
+            'time': request_obj['time'].strftime('%Y-%m-%d %H:%M:%S'),
+            'rate': request_obj['rate']
+        }
+        for request_obj in last_requests
+    ]
+
+    response_data = {
         'current_rate': float(rate),
-        'last_requests': list(last_requests)
-    })
+        'last_requests': last_requests_formatted
+    }
+
+    return JsonResponse(response_data, json_dumps_params={'indent': 2})
