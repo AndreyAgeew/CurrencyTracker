ffb81858413367d192d2574f07c07d030d848a17
Andrey Ageew <faceofdeviler@gmail.com>
2024-01-23 12:43:37 +0300

    Adding a model to store information about exchange rate requests

--- RateFetcher/models.py
+++ RateFetcher/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CurrencyRequest(models.Model):
+    """
+    Модель для хранения информации о запросах курса валют.
+    """
+    time = models.DateTimeField(auto_now_add=True)
+    rate = models.FloatField()
+
+    def __str__(self):
+        """
+        Строковое представление модели.
+        """
+        return f"{self.time} - {self.rate}"
